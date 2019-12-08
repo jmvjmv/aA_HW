6db28d2fcb980da99da857efbd9862c09f07c240
jmvjmv <jmv8928@gmail.com>
2019-12-08 16:08:12 -0500

    Added a bfs method

--- W4D1/graph_nodes_hw.rb
+++ W4D1/graph_nodes_hw.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class GraphNode
 
     attr_accessor :value, :neighbors
@@ -8,11 +10,39 @@ class GraphNode
     end
 
     def inspect
-        @neighbors.inspect
+        @value.inspect
     end
 
+    def bfs(starting_node, target_value) #BREADth first search hehe
+        arr = [starting_node]
+        # visited = []
+        visited = Set.new
 
+        while arr.length > 0 
+            current_node = arr.shift
+            unless visited.include?(current_node)
+                return current_node.value if current_node.value == target_value
+                # visited += current_node
+                visited.add(current_node)
+                arr += current_node.neighbors
+            end
+        end
+        nil
+    end
 
+    # this is from the classwork on friday just using it as reference
+    # def bfs(target)
+    #     arr = [self]
+
+    #     while arr.length > 0
+    #         current_node = arr.shift
+    #         if current_node.value == target
+    #             return current_node
+    #         end
+    #         arr += current_node.children
+    #     end
+    #     nil
+    # end
 end
 
 
@@ -27,5 +57,6 @@ c.neighbors = [b, d]
 e.neighbors = [a]
 f.neighbors = [e]
 
-p a
-p c
+# p a.neighbors
+p a.bfs(a,"b")
+p a.bfs(a, "f")
