aebe50b09c45096e03bdc77d28ea878e80b2fd4c
jmvjmv <jmv8928@gmail.com>
2019-12-10 07:21:24 -0500

    Added Board#valid_move?

--- W4D2/skeleton 2/lib/board.rb
+++ W4D2/skeleton 2/lib/board.rb
@@ -2,13 +2,27 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) {Array.new}
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      if i == 6 || i == 13
+        next
+      end
+      4.times do 
+        cup << :stone
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    raise 'Invalid starting cup' if start_pos < 0 || start_pos > 12
+    raise 'Starting cup is empty' if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
