06feaa74efab448be37a1fd44181aa28688793ac
jmvjmv <jmv8928@gmail.com>
2019-12-11 00:38:09 -0500

    Drew the rest of the owl

--- W4D3/skeleton 2/lib/simon.rb
+++ W4D3/skeleton 2/lib/simon.rb
@@ -4,19 +4,32 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    until game_over
+      take_turn
+    end
 
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    show_sequence
+    require_sequence
 
+    unless game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +37,7 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.shuffle[0]
   end
 
   def round_success_message
@@ -36,6 +49,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
