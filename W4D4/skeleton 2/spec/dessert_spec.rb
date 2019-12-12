ed19b22099287cd54180fed50b2b93f156da90a1
jmvjmv <jmv8928@gmail.com>
2019-12-11 19:45:46 -0500

    did everything but the last method

--- W4D4/skeleton 2/spec/dessert_spec.rb
+++ W4D4/skeleton 2/spec/dessert_spec.rb
@@ -6,31 +6,62 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef",name:"Phil") }
+  let(:flan) {Dessert.new("flan", 20,chef)}
+  
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do 
+      expect(flan.type).to eq("flan")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do 
+      expect(flan.quantity).to eq(20)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do 
+      expect(flan.ingredients).to eq([])
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do 
+      expect {Dessert.new("Air", "fton", chef)}.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do 
+      flan.add_ingredient("Devil's cocaine")
+      expect(flan.ingredients).to include("Devil's cocaine")
+    end
+
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients = ["Devil's lettuce", "Eggs", "child","sugar"]
+
+      ingredients.each do |ingredient|
+      flan.add_ingredient(ingredient)
+      end
+
+      expect(flan.ingredients).to eq(ingredients)
+      flan.mix!
+      expect(flan.ingredients).not_to eq(ingredients)
+      expect(flan.ingredients.sort).to eq(ingredients.sort)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do 
+      flan.eat(1)
+      expect(flan.quantity).to eq(19)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { flan.eat(60)}.to raise_error("not enough left!")
+    end
   end
+
 
   describe "#serve" do
     it "contains the titleized version of the chef's name"
